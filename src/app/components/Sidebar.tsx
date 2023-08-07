3823bf60e3b559269372034d649142e57d5c50e9
Douglas Rios <glinhas55@hotmail.com>
2023-08-07 13:03:08 -0300

    Finalizando sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -7,17 +7,18 @@ type Props = {}
 
 export default function Sidebar({}: Props) {
   return (
-    <aside className='overflow-hidden bg-red-900 w-20'>
-      <Image className='object-cover rounded-full' src='https://avatars.githubusercontent.com/u/87489750?v=4' width={100} height={100} alt='' />
-      <div className='grid'>
-        <strong>
+    <aside className='overflow-hidden bg-stone-900 w-64 grid justify-items-center rounded-xl'>
+      <Image className='object-cover w-full h-16' src='https://avatars.githubusercontent.com/u/87489750?v=4' width={100} height={100} alt='' />
+      <Image className='object-cover rounded-full border-2 border-green-700' src='https://avatars.githubusercontent.com/u/87489750?v=4' width={100} height={100} alt='' />
+      <div className='grid justify-items-center'>
+        <strong className='mt-2 text-xl font-bold'>
           Douglas Rios
         </strong>
-        <span>
+        <span className='text-sm'>
           Web Developer
         </span>
       </div>
-      <footer className='flex'>
+      <footer className='flex items-center space-x-2 p-2 border-2 rounded-xl border-green-700 text-green-600 m-3'>
         <BsFillPenFill />
         <Link href='' >Editar seu perfil</Link>
       </footer>
