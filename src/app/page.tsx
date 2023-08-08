8e3a9114fd41988a16f97a82b0cffab9054b08a4
Douglas Rios <glinhas55@hotmail.com>
2023-08-08 13:00:25 -0300

    Finalizando post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-import Image from 'next/image'
+import Image from "next/image";
 import { FiCoffee } from "react-icons/fi";
-import Sidebar from './components/Sidebar';
-import Post from './components/Post';
-
-
+import Sidebar from "./components/Sidebar";
+import Post from "./components/Post";
 
 export default function Home() {
   return (
-    <div className='w-full h-full'>
+    <div className="w-full h-full">
       <header className="flex justify-center items-center bg-stone-900 h-24">
-        <FiCoffee className='mr-4 h-7 w-7' />
-        <p className='font-bold text-2xl'>Coffee Talks</p>
+        <FiCoffee className="mr-4 h-7 w-7" />
+        <p className="font-bold text-2xl">Coffee Talks</p>
       </header>
-      <main className='flex'>
+      <main className="flex p-5 space-x-5">
         <Sidebar />
-        <body>
+        <div className=" w-full bg-stone-900 grid justify-items-center content-center rounded-xl p-2">
           <Post />
-        </body>
+        </div>
       </main>
     </div>
-  )
+  );
 }
